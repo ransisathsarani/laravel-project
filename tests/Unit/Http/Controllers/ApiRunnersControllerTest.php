82a16fff1c8e49afc09685955132db54d57d42e8
sathsaranir1996 <ransiambagahage@gmail.com>
2021-08-27 16:40:24 +0530

    minor fix

--- tests/Unit/Http/Controllers/ApiRunnersControllerTest.php
+++ tests/Unit/Http/Controllers/ApiRunnersControllerTest.php
@@ -7,25 +7,5 @@ use PHPUnit\Framework\TestCase;
 
 class ApiRunnersControllerTest extends TestCase
 {
-    public function testGettingRunnerDetail()
-    {
-        $response = $this->json('GET', '/api/v1/runner/{id}/form-data', );
-        $response->assertStatus(200);
-
-        $response->assertJsonStructure(
-            [
-                [
-                    'id',
-                    'name',
-                    'description',
-                    'units',
-                    'price',
-                    'image',
-                    'created_at',
-                    'updated_at'
-                ]
-            ]
-        );
-    }
 
 }
